0bd91b8db7e4747b7ca96a58c5ad71406bf61143
Yash Raj Gupta <yashrajgupta_co20b7_62@dtu.ac.in>
2022-10-29 11:46:31 +0530

    Made Services and Content Section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,134 +1,40 @@
 import React, { Fragment } from "react";
 import Header from "../Layout/Header/Header";
+import Banner from "../../assets/images/banner.png";
+
 import "./Home.css";
+
+import ContentContainer from "./Content/ContentContainer";
+import ServiceContainer from "./ServiceHome/ServiceContainer";
+import MobileHeader from "../Layout/Header/MobileHeader";
+import Top from "./Top";
+
 const Home = () => {
   const user = "Yash";
   const login = true;
   return (
     <Fragment>
-      <Header className="navbar" data={{ User: { user }, Login: {login}}} />
-      <div className="container">
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-        condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl, eget
-        aliquet nisl nisl sit amet nisl. Donec euismod, nisl ut aliquam
-        lacinia, nisl nisl aliquet nisl, eget aliquet nisl nisl sit amet
-        nisl. Donec euismod, nisl ut aliquam lacinia, nisl nisl aliquet
-        
+      <div className="Container" id="top">
+        <Header
+          className="Navbar"
+          data={{ User: { user }, Login: { login } }}
+        />
+        <MobileHeader
+          className="MobileNavbar"
+          data={{ User: { user }, Login: { login } }}
+        />
+        <div className="MainContainer">
+          <hr className="hr" />
+          <div className="BannerContainer">
+            <div className="Banner">
+              <img src={Banner} className="temp" alt="Banner" />
+            </div>
+            <hr />
+          </div>
+          <ContentContainer />
+          <ServiceContainer />
+          <Top className="ScrollTop" />
+        </div>
       </div>
     </Fragment>
   );
--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -2,21 +2,10 @@ import React, { Fragment } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../../../assets/images/logo.jpg";
 import "./Header.css";
-import { FaUserAlt } from "react-icons/fa";
+import NavLinks from "./NavLinks";
 
 const Header = (props) => {
   const navigate = useNavigate();
-
-  const links = [
-    { name: "Home", url: "/" },
-    { name: "About Us", url: "/about" },
-    { name: "Services", url: "/services" },
-    { name: "My Bookings", url: "/bookings" },
-    {
-      name: props.data.Login.login ? `Hello ${props.data.User.user}` : "Login",
-      url: props.data.Login.login ? "/profile" : "/login",
-    },
-  ];
 
   return (
     <Fragment>
@@ -33,20 +22,7 @@ const Header = (props) => {
           <h1 className="LogoText">SolarVerse</h1>
         </div>
         <div className="RightContainer">
-          <ul className="NavLinks">
-            {links.map(({ name, url }) => {
-              return (
-                <li key={name}>
-                  <Link to={url}>{name}</Link>
-                </li>
-              );
-            })}
-            <li>
-              <Link to={props.data.Login.login ? "/profile" : "/login"}>
-                {<FaUserAlt />}
-              </Link>
-            </li>
-          </ul>
+          <NavLinks data={props.data}/>
         </div>
       </div>
     </Fragment>
